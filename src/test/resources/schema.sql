5b6581c0f0f847581beea9a0704c598d1e171d18
xjahic <jahic.jakub@gmail.com>
2019-04-22 18:28:29 +0200

    Otestovanie DB modulu - Test pre zapis - H2

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,30 @@
+
+DROP TABLE IF EXISTS customer;
+CREATE TABLE customer (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  surname varchar(45) NOT NULL,
+  email varchar(45) NOT NULL,
+  address varchar(45) NOT NULL,
+  age int(11) DEFAULT NULL,
+  phone_number varchar(45) DEFAULT NULL
+);
+
+DROP TABLE IF EXISTS merchant;
+CREATE TABLE merchant (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  email varchar(45) NOT NULL,
+  address varchar(45) NOT NULL
+);
+
+DROP TABLE IF EXISTS product;
+CREATE TABLE product (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  merchant_id int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(80) NOT NULL,
+  price double NOT NULL,
+  created_at datetime NOT NULL,
+  available int(11) NOT NULL
+);
