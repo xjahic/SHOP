6f4d94ec106e5151209097af1b7d48c2cb1a954f
xjahic <jahic.jakub@gmail.com>
2019-04-22 17:25:47 +0200

    Otestovanie DB modulu - Test pre zapis - Product

--- src/test/java/com/learn2code/Shop/DBInsertTests.java
+++ src/test/java/com/learn2code/Shop/DBInsertTests.java
@@ -2,6 +2,7 @@ package com.learn2code.Shop;
 
 import com.learn2code.Shop.domain.Customer;
 import com.learn2code.Shop.domain.Merchant;
+import com.learn2code.Shop.domain.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,9 +23,36 @@ public class DBInsertTests {
 
     private final String insertCustomer = "INSERT INTO customer(name, surname, email, address, age, phone_number) values (?, ?, ?, ?, ?, ?)";
     private final String insertMerchant = "INSERT INTO merchant(name, email, address) values (?, ?, ?)";
+    private final String insertProduct = "INSERT INTO product(merchant_id, name, description, price, created_at, available) values (?, ?, ?, ?, ?, ?)";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void createProduct() {
+        Product product = new Product();
+        product.setMerchantId(1);
+        product.setName("Klavesnica");
+        product.setDescription("Velmi dobra klavesnica");
+        product.setPrice(15.5);
+        product.setCreatedAt(Timestamp.from(Instant.now()));
+        product.setAvailable(10);
+
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement(insertProduct);
+                ps.setInt(1, product.getMerchantId());
+                ps.setString(2, product.getName());
+                ps.setString(3, product.getDescription());
+                ps.setDouble(4, product.getPrice());
+                ps.setTimestamp(5, product.getCreatedAt());
+                ps.setInt(6, product.getAvailable());
+                return ps;
+            }
+        });
+
+    }
 
     @Test
     public void createCustomer() {
