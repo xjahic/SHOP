86f8327d3fc29d780339a4d590da07dd0ffa43d9
xjahic <jahic.jakub@gmail.com>
2019-04-28 08:58:39 +0200

    Implementacia databazovych rozhrani - CustomerService implementacia

---
+++ src/main/java/com/learn2code/Shop/db/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,33 @@
+package com.learn2code.Shop.db.service.impl;
+
+import com.learn2code.Shop.db.repository.CustomerRepository;
+import com.learn2code.Shop.db.service.api.CustomerService;
+import com.learn2code.Shop.domain.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> getCustomer() {
+        return customerRepository.getAll();
+    }
+
+    @Override
+    public Customer get(int id) {
+        return customerRepository.get(id);
+    }
+
+    @Override
+    public Integer add(Customer customer) {
+        return customerRepository.add(customer);
+    }
+}
