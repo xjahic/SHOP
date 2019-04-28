a05e95245b1f69f5a4eeaa10a8f9a65977007fad
xjahic <jahic.jakub@gmail.com>
2019-04-28 10:49:27 +0200

    Implementacia databazovych rozhrani - MerchantService implementacia

---
+++ src/main/java/com/learn2code/Shop/db/service/impl/MerchantServiceImpl.java
@@ -0,0 +1,33 @@
+package com.learn2code.Shop.db.service.impl;
+
+import com.learn2code.Shop.db.repository.MerchantRepository;
+import com.learn2code.Shop.db.service.api.MerchantService;
+import com.learn2code.Shop.domain.Merchant;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MerchantServiceImpl implements MerchantService {
+
+    private final MerchantRepository merchantRepository;
+
+    public MerchantServiceImpl(MerchantRepository merchantRepository) {
+        this.merchantRepository = merchantRepository;
+    }
+
+    @Override
+    public List<Merchant> getMerchants() {
+        return merchantRepository.getAll();
+    }
+
+    @Override
+    public Merchant get(int id) {
+        return merchantRepository.get(id);
+    }
+
+    @Override
+    public Integer add(Merchant merchant) {
+        return merchantRepository.add(merchant);
+    }
+}
