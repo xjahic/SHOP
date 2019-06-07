cce96668ad2e9b458c8aa0a9faa991c3d66dd97e
xjahic <jahic.jakub@gmail.com>
2019-06-07 22:18:13 +0200

    Implementacia nakupu 1.cast - CustomerAccountServiceImpl

---
+++ src/main/java/com/learn2code/Shop/db/service/impl/CustomerAccountServiceImpl.java
@@ -0,0 +1,31 @@
+package com.learn2code.Shop.db.service.impl;
+
+import com.learn2code.Shop.db.repository.CustomerAccountRepository;
+import com.learn2code.Shop.db.service.api.CustomerAccountService;
+import com.learn2code.Shop.domain.CustomerAccount;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerAccountServiceImpl implements CustomerAccountService {
+
+    private final CustomerAccountRepository customerAccountRepository;
+
+    public CustomerAccountServiceImpl(CustomerAccountRepository customerAccountRepository) {
+        this.customerAccountRepository = customerAccountRepository;
+    }
+
+    @Override
+    public void addCustomerAccount(CustomerAccount customerAccount) {
+        customerAccountRepository.add(customerAccount);
+    }
+
+    @Override
+    public Double getMoney(int customerId) {
+        return customerAccountRepository.getMoney(customerId);
+    }
+
+    @Override
+    public void setMoney(int customerId, double money) {
+        customerAccountRepository.setMoney(customerId, money);
+    }
+}
