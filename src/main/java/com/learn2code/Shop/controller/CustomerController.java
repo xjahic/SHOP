bfda2ba13a6774c38418b2d37921dd02d0cefce4
xjahic <jahic.jakub@gmail.com>
2019-06-07 22:18:31 +0200

    Implementacia nakupu 1.cast - CustomerController rozsireni o pridanie uctu

--- src/main/java/com/learn2code/Shop/controller/CustomerController.java
+++ src/main/java/com/learn2code/Shop/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.learn2code.Shop.controller;
 
+import com.learn2code.Shop.db.service.api.CustomerAccountService;
 import com.learn2code.Shop.db.service.api.CustomerService;
 import com.learn2code.Shop.domain.Customer;
+import com.learn2code.Shop.domain.CustomerAccount;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +15,11 @@ import java.util.List;
 public class CustomerController {
 
     private final CustomerService customerService;
+    private final CustomerAccountService customerAccountService;
 
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, CustomerAccountService customerAccountService) {
         this.customerService = customerService;
+        this.customerAccountService = customerAccountService;
     }
 
     @PostMapping
@@ -40,5 +44,11 @@ public class CustomerController {
     public ResponseEntity getAll() {
         List<Customer> customerList = customerService.getCustomers();
         return new ResponseEntity<>(customerList, HttpStatus.OK);
+    }
+
+    @PostMapping("/account")
+    public ResponseEntity addAccount(@RequestBody CustomerAccount customerAccount) {
+        customerAccountService.addCustomerAccount(customerAccount);
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
     }
 }
