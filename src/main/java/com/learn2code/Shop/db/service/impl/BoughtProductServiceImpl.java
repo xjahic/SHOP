34b4f670189d7005c26281a8825ea39773d79f1d
xjahic <jahic.jakub@gmail.com>
2019-06-08 11:29:21 +0200

    Implementacia nakupu 2.cast - BoughtProductServiceImpl

---
+++ src/main/java/com/learn2code/Shop/db/service/impl/BoughtProductServiceImpl.java
@@ -0,0 +1,28 @@
+package com.learn2code.Shop.db.service.impl;
+
+import com.learn2code.Shop.db.repository.BoughtProductRepository;
+import com.learn2code.Shop.db.service.api.BoughtProductService;
+import com.learn2code.Shop.domain.BoughtProduct;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoughtProductServiceImpl implements BoughtProductService {
+
+    private final BoughtProductRepository boughtProductRepository;
+
+    public BoughtProductServiceImpl(BoughtProductRepository boughtProductRepository) {
+        this.boughtProductRepository = boughtProductRepository;
+    }
+
+    @Override
+    public void add(BoughtProduct boughtProduct) {
+        boughtProductRepository.add(boughtProduct);
+    }
+
+    @Override
+    public List<BoughtProduct> getAllByCustomerId(int customerId) {
+        return boughtProductRepository.getAllByCustomerId(customerId);
+    }
+}
